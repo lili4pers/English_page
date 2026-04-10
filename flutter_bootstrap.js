719e2fbeb130652e13fd44f3a2108b9ef9fc5984
Linda Pérez <lperezd12@miumg.edu.gt>
2026-04-10 15:20:38 -0600

    fix base href

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "250307797"
+    serviceWorkerVersion: "2007659176"
   }
 });
